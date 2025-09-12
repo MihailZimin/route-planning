40b34a70f7647e3805b6a1dc1a18e33d2ca5c995
Kirikus <kirikusan@gmail.com>
2025-09-12 10:09:07 +0300

    CMake: added core target
    Library with some data.

---
+++ core/data.cpp
@@ -0,0 +1,5 @@
+#include "data.h"
+
+float array[N] = {
+    0, 1, 2, 3, 4, 0, 1, 2, 3, 4,
+};
---
+++ core/data.h
@@ -0,0 +1,7 @@
+#ifndef DATA_H
+#define DATA_H
+
+constexpr unsigned int N = 10;
+extern float array[N];
+
+#endif  // DATA_H
