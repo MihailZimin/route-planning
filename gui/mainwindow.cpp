cb460c29dae93e9e8c1996d5dbb6d7d8600894a7
Kirikus <kirikusan@gmail.com>
2025-09-12 10:26:23 +0300

    gui: added example plot

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -1,15 +1,27 @@
 #include "mainwindow.h"
+
+#include "../core/data.h"
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-  : QMainWindow(parent)
-  , ui(new Ui::MainWindow)
-{
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
+
+  QVector<double> x(N), y(N);  // initialize with entries 0..100
+  for (int i = 0; i < N; ++i) {
+    x[i] = i;
+    y[i] = array[i];
+  }
+  // create graph and assign data to it:
+  ui->plot->addGraph();
+  ui->plot->graph(0)->setData(x, y);
+  // give the axes some labels:
+  ui->plot->xAxis->setLabel("x");
+  ui->plot->yAxis->setLabel("y");
+  // set axes ranges, so we see all data:
+  ui->plot->xAxis->setRange(0, 9);
+  ui->plot->yAxis->setRange(0, 4);
+  ui->plot->replot();
 }
 
-MainWindow::~MainWindow()
-{
-  delete ui;
-}
-
+MainWindow::~MainWindow() { delete ui; }
