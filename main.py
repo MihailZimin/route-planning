9b73c24fc78ad49933dcf32e36f007a2b5f1ca5d
NikGroshev <nikita.groshev2006@gmail.com>
2025-10-10 00:24:29 +0300

    add drawer classes, add realization for point, circle and line drawer

---
+++ main.py
@@ -0,0 +1,17 @@
+"""Entry point.
+
+This is the entrance to the app.
+"""
+
+
+import sys
+
+from PyQt6.QtWidgets import QApplication
+
+from main_window.mainwindow import MainWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    app.exec()
