ccf1733a44402d0f11472e7be8189483ace8f402
Mikhail <snowzzz2006@gmail.com>
2025-10-20 15:32:47 +0300

    Add brute force algorithm of TSP

---
+++ tsp_algorithms/tsp_brute_force.py
@@ -0,0 +1,55 @@
+"""Brute force algorithm class."""
+import itertools
+
+import numpy as np
+
+from .tsp_abstract_solver import TSPSolver
+
+
+class BruteForceSolver(TSPSolver):
+    """
+    Class that represents brute force solution to TSP.
+    """
+
+    def solve(self, data: np.ndarray, start: int) -> list[int]:
+        """
+        Solve travel salesman problem.
+
+        Args:
+            data: matrix of lengthes, where data[i][j] is length of path from i-th control point to j-th
+            start: index of start control point
+
+        Returns:
+            sequence(list) of points in optimal order.
+
+        """
+        size = data.shape[0]
+
+        permutated_vertices = [i for i in range(size) if i != start]
+
+        optimal_length = np.inf
+        optimal_path = []
+
+        for permutation in itertools.permutations(permutated_vertices):
+            cur_length = 0
+            prev_vertex = start
+            for vertex in permutation:
+                cur_length += data[prev_vertex, vertex]
+                prev_vertex = vertex
+            cur_length += data[permutation[-1], start]
+            if cur_length < optimal_length:
+                optimal_length = cur_length
+                optimal_path = [start, *permutation, start]
+
+        self._optimal_length = optimal_length
+        return optimal_path
+    
+    def get_optimal_length(self) -> float:
+        """
+        Method for debug
+
+        Returns:
+            most optimal length of route
+
+        """
+        return self._optimal_length
