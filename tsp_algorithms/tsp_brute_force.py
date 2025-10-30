95d4ef24e272d16caa4884ab04e4e148a4ac1008
Mikhail <snowzzz2006@gmail.com>
2025-10-30 21:03:38 +0300

    Change variable names in brute force class

--- tsp_algorithms/tsp_brute_force.py
+++ tsp_algorithms/tsp_brute_force.py
@@ -11,19 +11,21 @@ class BruteForceSolver(TSPSolver):
     Class that represents brute force solution to TSP.
     """
 
-    def solve(self, data: np.ndarray, start: int) -> list[int]:
+    def solve(self, matrix: np.ndarray, start: int) -> list[int]:
         """
         Solve travel salesman problem.
 
         Args:
-            data: matrix of lengthes, where data[i][j] is length of path from i-th control point to j-th
+            matrix: matrix of lengthes,
+                where matrix[i][j] is length of path from i-th control point to j-th
             start: index of start control point
 
         Returns:
             sequence(list) of points in optimal order.
 
         """
-        size = data.shape[0]
+        size = matrix.shape[0]
+        matrix = np.where(matrix == -1, np.inf, matrix)
 
         permutated_vertices = [i for i in range(size) if i != start]
 
@@ -34,9 +36,9 @@ class BruteForceSolver(TSPSolver):
             cur_length = 0
             prev_vertex = start
             for vertex in permutation:
-                cur_length += data[prev_vertex, vertex]
+                cur_length += matrix[prev_vertex, vertex]
                 prev_vertex = vertex
-            cur_length += data[permutation[-1], start]
+            cur_length += matrix[permutation[-1], start]
             if cur_length < optimal_length:
                 optimal_length = cur_length
                 optimal_path = [start, *permutation, start]
