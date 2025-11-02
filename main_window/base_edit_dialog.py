b180c770dc35fa46f4e01d49e7151c2cbc665dea
NikGroshev <nikita.groshev2006@gmail.com>
2025-11-02 12:48:17 +0300

    add base class for editing geo objects

---
+++ main_window/base_edit_dialog.py
@@ -0,0 +1,60 @@
+"""
+Base edit window class.
+
+This module provides:
+- base class EditDialogWindow for editing geometry object.
+
+"""
+
+
+from PyQt6 import uic
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import QDialog, QWidget
+
+from draw.abstract_drawer import ABCDrawer
+
+
+class EditDialogWindow(QDialog):
+    """
+    Edit window class for editing geo objects.
+    """
+
+    def __init__(self, geo_object : ABCDrawer, path: str, parent: QWidget = None) -> None:
+        """
+        Create edit window object.
+
+        Args:
+
+            geo_object: geometry object which will be edited.
+            path: path to the ui file with dialog window for current object.
+            parent: parent window.
+        """
+        super().__init__(parent)
+        self._geo_object = geo_object
+        self.InitializeWin(path)
+        self.loadParams()
+
+    def InitializeWin(self, path: str) -> None:
+        """
+        Initialize edit window.
+
+        Args:
+
+            path: path to the ui file with dialog window.
+        """
+        uic.loadUi(path, self)
+        self.setMinimumSize(QSize(400, 210))
+        self.buttonBox.accepted.disconnect()
+        self.buttonBox.accepted.connect(self.validateAccept)
+        self.buttonBox.rejected.connect(self.reject)
+        self.loadParams()
+
+    def validateAccept() -> None:
+        """
+        Slot for accept button with validation of object parameters.
+        """
+
+    def loadParams(self) -> None:
+        """
+        Load current parameters of geo object.
+        """
