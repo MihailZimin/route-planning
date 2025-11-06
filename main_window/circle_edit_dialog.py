57f98200b7aeb6cda69112a8bc573a6183002eeb
NikGroshev <nikita.groshev2006@gmail.com>
2025-11-06 19:15:49 +0300

    add circle edit dialog window class

---
+++ main_window/circle_edit_dialog.py
@@ -0,0 +1,75 @@
+"""
+Circle edit window class.
+
+This module provides:
+- CircleEditDialogWindow for editing circle.
+
+"""
+
+from main_window.base_edit_dialog import EditDialogWindow
+
+from draw.circle_drawer import CircleDrawer
+
+from PyQt6.QtWidgets import QWidget, QMessageBox
+
+
+class CircleEditDialogWindow(EditDialogWindow):
+    """
+    Class for edit circle window.
+    """
+
+    def __init__(self, point: CircleDrawer, parent: QWidget = None) -> None:
+        """
+        Create circle edit window.
+        """
+        super().__init__(point, "main_window/circle_edit_dialog.ui", parent)
+
+    def validateAccept(self) -> None:
+        """
+        Slot for accept button with validation of parameters.
+        """
+        x_coord = self.xLineEdit.text()
+        y_coord = self.yLineEdit.text()
+        radius = self.radiusLineEdit.text()
+        if not x_coord or not y_coord:
+            QMessageBox.information(self, "Траектория БПЛА", "Заполните все поля")
+            return
+        try:
+            x_coord = float(x_coord)
+            y_coord = float(y_coord)
+            rad = float(radius)
+        except ValueError:
+            QMessageBox.information(self, "Траектория БПЛА", "Введите корректные параметры")
+            return
+        if x_coord < 0 or x_coord > 1000:
+            QMessageBox.information(self, "Траектория БПЛА", "Введите корректные координаты")
+            return
+        if y_coord < 0 or y_coord > 1000:
+            QMessageBox.information(self, "Траектория БПЛА", "Введите корректные координаты")
+            return
+        if rad < 0 or rad > 500:
+            QMessageBox.information(self, "Траектория БПЛА", "Введите корректный радиус")
+            return
+        self.accept()
+
+    def loadParams(self) -> None:
+        """
+        Load circle parameters.
+        """
+        self.nameLineEdit.setText(self._geo_object.parameters["Название"])
+        self.xLineEdit.setText(str(self._geo_object.parameters["X"]))
+        self.yLineEdit.setText(str(self._geo_object.parameters["Y"]))
+        self.radiusLineEdit.setText(str(self._geo_object.parameters["Радиус"]))
+
+    def setChanges(self) -> None:
+        """
+        Change circle parameters.
+        """
+        name = self.nameLineEdit.text()
+        x = float(self.xLineEdit.text())
+        y = float(self.yLineEdit.text())
+        r = float(self.radiusLineEdit.text())
+        self._geo_object.name = name
+        self._geo_object.center.x = x
+        self._geo_object.center.y = y
+        self._geo_object.radius = r
