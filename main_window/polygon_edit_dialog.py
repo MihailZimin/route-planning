ba60d2052cc8213196290838e4366ceefcd846e2
NikGroshev <nikita.groshev2006@gmail.com>
2025-11-06 21:00:11 +0300

    add polygon edit dialog window class

---
+++ main_window/polygon_edit_dialog.py
@@ -0,0 +1,85 @@
+"""
+Polygon edit dialog class
+
+This module provides:
+- PolygonEditDialogWindow for editing polygon.
+
+"""
+
+from main_window.base_edit_dialog import EditDialogWindow
+
+from PyQt6.QtWidgets import QWidget, QMessageBox, QLineEdit
+
+from draw.polygon_drawer import PolygonDrawer
+
+class PolygonEditDialogWindow(EditDialogWindow):
+    """
+    Class for edit polygon window
+    """
+
+    def __init__(self, geo_object: PolygonDrawer, parent: QWidget = None) -> None:
+        """
+        Create polygon edit window.
+        """
+        self._rows: dict = {}
+        super().__init__(geo_object, "main_window/polygon_edit_dialog.ui", parent)
+
+    def InitializeWin(self, path):
+        """
+        Initialize polygon edit dialog window.
+
+        Args:
+            path: path to the ui file.
+ 
+        """
+        super().InitializeWin(path)
+        self._rows["Название"] = QLineEdit()
+        self.formLayout.addRow("Название", self._rows["Название"])
+        for i in range(len(self._geo_object.points)):
+            self._rows["X" + str(i + 1)] = QLineEdit()
+            self._rows["Y" + str(i + 1)] = QLineEdit()
+            self.formLayout.addRow("X" + str(i + 1), self._rows["X" + str(i + 1)])
+            self.formLayout.addRow("Y" + str(i + 1), self._rows["Y" + str(i + 1)])
+
+    def validateAccept(self) -> None:
+        """
+        Slot for accept button with validation of parameters.
+        """
+        for i in range(len(self._geo_object.points)):
+            x_coord = self._rows["X" + str(i + 1)].text()
+            y_coord = self._rows["Y" + str(i + 1)].text()
+
+            if not x_coord or not y_coord:
+                QMessageBox.information(self, "Траектория БПЛА", "Заполните все поля")
+                return
+            try:
+                x_coord = float(x_coord)
+                y_coord = float(y_coord)
+            except ValueError:
+                QMessageBox.information(self, "Траектория БПЛА", "Введите корректные координаты")
+                return
+            if x_coord < 0 or x_coord > 1000:
+                QMessageBox.information(self, "Траектория БПЛА", "Введите корректные координаты")
+                return
+            if y_coord < 0 or y_coord > 1000:
+                QMessageBox.information(self, "Траектория БПЛА", "Введите корректные координаты")
+                return
+        self.accept()
+
+    def loadParams(self) -> None:
+        """
+        Load polygon parameters.
+        """
+        self._rows["Название"].setText(self._geo_object.name)
+        for i, point in enumerate(self._geo_object.points):
+            self._rows["X" + str(i + 1)].setText(str(point.x))
+            self._rows["Y" + str(i + 1)].setText(str(point.y))
+
+    def setChanges(self) -> None:
+        """
+        Change polygon parameters.
+        """
+        self._geo_object.name = self._rows["Название"].text()
+        for i, point in enumerate(self._geo_object.points):
+            point.x = float(self._rows["X" + str(i + 1)].text())
+            point.y = float(self._rows["Y" + str(i + 1)].text())
