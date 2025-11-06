7f17b2886334bc113bed608a590001c181240982
Mikhail <snowzzz2006@gmail.com>
2025-11-06 19:29:54 +0300

    Adjust point logic to point validator class

--- core/point.py
+++ core/point.py
@@ -2,7 +2,7 @@
 import json
 
 from .abstract_geometry import ABCGeo
-from .data_validator import GeometryValidator
+from .data_validation.point_validator import PointValidator
 
 
 class Point(ABCGeo):
@@ -10,7 +10,7 @@ class Point(ABCGeo):
     Point core class.
     """
 
-    @GeometryValidator.validate("Point_init")
+    @PointValidator.validate("Point_init")
     def __init__(self, x: float = 0, y: float = 0) -> None:
         """
         Initialize 2D point.
@@ -51,7 +51,7 @@ class Point(ABCGeo):
         return self._x
 
     @x.setter
-    @GeometryValidator.validate("Point_x_setter")
+    @PointValidator.validate("Point_x_setter")
     def x(self, x_coord: float) -> None:
         """
         Set x coordinate of point.
@@ -70,7 +70,7 @@ class Point(ABCGeo):
         return self._y
 
     @y.setter
-    @GeometryValidator.validate("Point_y_setter")
+    @PointValidator.validate("Point_y_setter")
     def y(self, y_coord: float) -> None:
         """
         Set y coordinate of point.
