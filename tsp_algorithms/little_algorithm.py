7220932e39d0f5282f62e23f914b0fb0ee6c7f99
NikGroshev <nikita.groshev2006@gmail.com>
2025-11-20 23:08:12 +0300

    fix input matrix changes

--- tsp_algorithms/little_algorithm.py
+++ tsp_algorithms/little_algorithm.py
@@ -321,7 +321,9 @@ class LittleAlgorithm(TSPSolver):
             list of indices of points which form circle for the most optimal TSP solution
 
         """
+        np.fill_diagonal(matrix, np.inf)
         matrix = np.where(matrix == -1, np.inf, matrix)
+        print(matrix)
         sz = matrix.shape[0]
         nodes = []
 
